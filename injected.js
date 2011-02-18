18e5e606f68928b6b56e6465d4a31b379595d0b6
Gregory Durelle <g.durelle@studiomelipone.eu>
2011-02-18 17:50:14 +0100

    activating analytics

--- injected.js
+++ injected.js
@@ -103,18 +103,18 @@ function submission(){
 // GOOGLE ANALYTICS
 // /////////////////
 
-// var _gaq = _gaq || [];
-// _gaq.push(['_setAccount', 'UA-8358011-13']);
-// _gaq.push(['_trackPageview']);
-// 
-// (function() {
-//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//   ga.src = 'https://ssl.google-analytics.com/ga.js';
-//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-// })();
-// 
+var upshot_safari_gaq = upshot_safari_gaq || [];
+upshot_safari_gaq.push(['_setAccount', 'UA-8358011-13']);
+upshot_safari_gaq.push(['_trackPageview']);
+
+(function() {
+  var upshot_safari_ga = document.createElement('script'); upshot_safari_ga.type = 'text/javascript'; upshot_safari_ga.async = true;
+  upshot_safari_ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var upshot_safari_s = document.getElementsByTagName('script')[0]; upshot_safari_s.parentNode.insertBefore(upshot_safari_ga, upshot_safari_s);
+})();
+
 function trackButton(button_name) {
-  // _gaq.push(['_trackEvent', 'button_' + button_name, 'clicked']);
+  upshot_safari_gaq.push(['_trackEvent', 'button_' + button_name, 'clicked']);
   
   if(button_name==='draft' || button_name==='error_on_draft'){
     setTimeout(function() {
